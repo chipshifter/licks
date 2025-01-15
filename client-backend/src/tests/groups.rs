a0b01c1d60cc6fbd8fdc2be9e5cb5dcd4489836b
R-D-C-N <R-D-C-N@users.noreply.github.com>
2025-01-15 02:36:05 -0500

    finish an incomplete test

--- client-backend/src/tests/groups.rs
+++ client-backend/src/tests/groups.rs
@@ -126,7 +126,7 @@ mod tests {
     pub async fn three_person_conversation() {
         let (client, _rx) = Client::new();
         let alice_manager = client
-            .get_in_memory_profile("alicia keys")
+            .get_in_memory_profile("alice")
             .await
             .expect("server is open and registration works");
 
@@ -135,18 +135,18 @@ mod tests {
             .await
             .expect("keypackage should have uploaded");
 
-        let charli_manager = client
-            .get_in_memory_profile("charli xcx")
+        let charlie_manager = client
+            .get_in_memory_profile("charlie")
             .await
             .expect("server is open and registration works");
 
-        charli_manager
+        charlie_manager
             .upload_new_key_packages(1)
             .await
             .expect("keypackage should have uploaded");
 
         let bob_manager = client
-            .get_in_memory_profile("bob dylan")
+            .get_in_memory_profile("bob")
             .await
             .expect("server is open and registration works");
 
@@ -158,7 +158,9 @@ mod tests {
             .await
             .expect("group creation should have succeeded");
 
-        bob_manager
+        let bobs_group_id = bobs_group_ui.group_identifier;
+
+        let alice_welcome = bob_manager
             .create_new_welcome(
                 bobs_group_ui.group_identifier,
                 alice_manager.profile.get_account_id(),
@@ -166,6 +168,89 @@ mod tests {
             .await
             .expect("welcome should have been created");
 
-        todo!("finish writing this test")
+        alice_manager
+            .join_group_from_welcome_and_listen(&alice_welcome)
+            .await
+            .expect("group should have been made from welcome");
+
+        // Send a message to alice and wait for it to get to her
+        {
+            let bobs_first_message_content = Content::plain_text("Hello Alice, I'm going to add Charlie to this group".to_owned());
+
+            bob_manager
+                .send_application_message(&bobs_group_id, bobs_first_message_content.clone())
+                .await
+                .expect("application message should have been sent");
+            tokio::time::sleep(Duration::from_millis(500)).await;
+
+            let alice_message_log = alice_manager.message_log.lock().await;
+            let alice_message = alice_message_log.last().map(|v| &v.1);
+
+            let alice_message_content = alice_message.map(|mes| &mes.content);
+
+            assert_eq!(alice_message_content, Some(&bobs_first_message_content));
+        }
+
+        // Send a message to bob and wait for it get to him
+        {
+            let alices_first_message_content = Content::plain_text("Hello, Bob".to_owned());
+
+            alice_manager
+                .send_application_message(&bobs_group_id, alices_first_message_content.clone())
+                .await
+                .expect("application message should have been sent");
+            tokio::time::sleep(Duration::from_millis(500)).await;
+
+            let mut bob_message_log = bob_manager.message_log.lock().await;
+            let bob_message = bob_message_log.pop().map(|v| v.1);
+
+            let bob_message_content = bob_message.map(|mes| mes.content);
+
+            assert_eq!(bob_message_content, Some(alices_first_message_content));
+        }
+
+        // Charlie gets affed to the group
+        let charlie_welcome = bob_manager
+            .create_new_welcome(
+                bobs_group_ui.group_identifier,
+                charlie_manager.profile.get_account_id(),
+            )
+            .await
+            .expect("welcome should have been created");
+
+        charlie_manager
+            .join_group_from_welcome_and_listen(&charlie_welcome)
+            .await
+            .expect("group should have been made from welcome");
+        
+        // Send a message to bob and alice and wait for it get to them
+        {
+            let charlies_first_message_content = Content::plain_text("Hello, Alice and Bob".to_owned());
+
+            charlie_manager
+                .send_application_message(&bobs_group_id, charlies_first_message_content.clone())
+                .await
+                .expect("application message should have been sent");
+            tokio::time::sleep(Duration::from_millis(500)).await;
+            tokio::time::sleep(Duration::from_millis(500)).await;
+
+            let mut bob_message_log = bob_manager.message_log.lock().await;
+            let bob_message = bob_message_log.pop().map(|v| v.1);
+
+            let bob_message_content = bob_message.map(|mes| mes.content);
+            drop(bob_message_log);
+
+            let mut alice_message_log = alice_manager.message_log.lock().await;
+            let alice_message = alice_message_log.pop().map(|v| v.1);
+
+            let alice_message_content = alice_message.map(|mes| mes.content);
+            drop(alice_message_log);
+
+            assert_eq!(bob_message_content, Some(charlies_first_message_content.clone()));
+            assert_eq!(alice_message_content, Some(charlies_first_message_content));
+            
+        }
+        
+        
     }
 }
